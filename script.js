10885639c74f969b91c9439ea63d108b2a9eedf0
sbalayan1 <balayans2014@yahoo.com>
2021-05-10 13:02:06 -0700

    added key up and down functionality in game

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function moveDodgerLeft() {
     const left = parseInt(leftNumbers, 10);
 
     if (left > 0) {
-        dodger.style.left = (`${left - 10}px`);
+        dodger.style.left = (`${left - 20}px`);
     }
 }
 
@@ -18,9 +18,40 @@ function moveDodgerRight() {
     const right = parseInt(rightNumbers, 10);
 
     if (right < 360) {
-        dodger.style.left = (`${right + 10}px`);
+        dodger.style.left = (`${right + 20}px`);
     }
 }
+
+//define function moveDodger up
+function moveDodgerUp() {
+    const upNumbers = dodger.style.bottom.replace("px","");
+    const up = parseInt(upNumbers, 10);
+
+    if (up < 380) {
+        dodger.style.bottom = (`${up + 20}px`);
+        e.preventDefault();
+    }
+}
+
+//define function moveDodger down
+function moveDodgerDown() {
+    const downNumbers = dodger.style.bottom.replace("px","");
+    const down = parseInt(downNumbers, 10);
+
+    if (down > 0) {
+        dodger.style.bottom = (`${down - 20}px`);
+       
+    }
+}
+
+
+//addEventListener that calls the right and left functions when the event occurs
+document.addEventListener('keydown',function(event) {
+    if (event.key === 'ArrowLeft') {
+        console.log(event);
+        moveDodgerLeft();
+    }
+});
 
 document.addEventListener('keydown',function(event) {
     if (event.key === 'ArrowRight') {
@@ -30,8 +61,28 @@ document.addEventListener('keydown',function(event) {
 });
 
 document.addEventListener('keydown',function(event) {
-    if (event.key === 'ArrowLeft') {
+    if (event.key === 'ArrowUp') {
         console.log(event);
-        moveDodgerLeft();
+        moveDodgerUp();
+        e.stopPropagation();
+            e.preventDefault();  
+            e.returnValue = false;
+            e.cancelBubble = true;
+            return false;
     }
 });
+
+document.addEventListener('keydown',function(event) {
+    if (event.key === 'ArrowDown') {
+        console.log(event);
+        moveDodgerDown();
+       
+    }
+});
+
+//code that prevents the page from scrolling when keyup/keydown
+window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
